8cc06cfbe13b1b0e991ab962a9e0cd3a5c1d2359
Menyhárt Attila <you@example.com>
2020-08-18 20:56:45 +0200

    Telefonos nézet kezdetek

--- main.js
+++ main.js
@@ -27,15 +27,27 @@ function LunchDisplay(x) {
     document.getElementById("date").innerText = today + " " + day;
     document.getElementById("lunch").innerText = lunch[today];
 
+    document.getElementById("date-phone").innerText = today + " " + day;
+    document.getElementById("lunch-phone").innerText = lunch[today];
+
     if (today == "2020. 8. 11.") {
         document.getElementById("back").innerHTML = '<a></a>';
         document.getElementById("next").innerHTML = '<a class="useful" onclick="LunchDisplay(+1)"></a>';
+
+        document.getElementById("back-phone").innerHTML = '<a></a>';
+        document.getElementById("next-phone").innerHTML = '<a class="useful" onclick="LunchDisplay(+1)"></a>';
     } else if (today == lastDay) {
         document.getElementById("back").innerHTML = '<a class="useful" onclick="LunchDisplay(-1)"></a>';
         document.getElementById("next").innerHTML = '<a></a>';
+        
+        document.getElementById("back-phone").innerHTML = '<a class="useful" onclick="LunchDisplay(-1)"></a>';
+        document.getElementById("next-phone").innerHTML = '<a></a>';
     } else {
         document.getElementById("back").innerHTML = '<a class="useful" onclick="LunchDisplay(-1)"></a>';
         document.getElementById("next").innerHTML = '<a class="useful" onclick="LunchDisplay(+1)"></a>';
+        
+        document.getElementById("back-phone").innerHTML = '<a class="useful" onclick="LunchDisplay(-1)"></a>';
+        document.getElementById("next-phone").innerHTML = '<a class="useful" onclick="LunchDisplay(+1)"></a>';
     }
 }
 
