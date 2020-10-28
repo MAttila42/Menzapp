270b971ce506fa48cf91235f6cbdec7c251504ae
M-Attila <menyhart.atika@gmail.com>
2020-10-28 12:05:01 +0100

    menzappdater test 2

--- menzappdater.py
+++ menzappdater.py
@@ -6,16 +6,18 @@ import json
 url = "http://gek.hu/images/Nyugatmagyar/2020/Eattrend-2020-oktober-rteuv.pdf"
 r = requests.get(url, allow_redirects=True)
 pdfFileObj = open('Eattrend-2020-oktober-rteuv.pdf', 'wb').write(r.content)
-df = tabula.read_pdf("Eattrend-2020-oktober-rteuv.pdf", output_format="json")
+page1 = tabula.read_pdf("Eattrend-2020-oktober-rteuv.pdf", output_format="json", pages=1)
+page2 = tabula.read_pdf("Eattrend-2020-oktober-rteuv.pdf", output_format="json", pages=2)
 
 # print(json.dumps(df, indent=2))
 
-# df[<hét_index>]["data"][<4|5 (leves|főétel)>][<nap_index + 1>]["text"]
+# page1[<hét_index (0|1)>]["data"][<4|5 (leves|főétel)>][<nap_index + 1>]["text"]
+# page2[<hét_index (0|1)>]["data"][<4|5 (leves|főétel)>][<nap_index + 1>]["text"]
 
-ki = df[2]["data"][4][1]["text"].replace("\r", "\n")
+ki = page1[1]["data"][4][1]["text"].replace("\r", "\n")
 
 print(ki)
 
-# f = open("test.json", "w")
+# f = open("test2.json", "w")
 # f.write(json.dumps(df, indent=2))
 # f.close()
